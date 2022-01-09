bcf06309c32ce8087e4550c40cbcfb903c1c5ad9
General Zelgius <mattlow123@gmail.com>
2022-01-09 13:13:50 -0500

    player name and fight function begun in js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
